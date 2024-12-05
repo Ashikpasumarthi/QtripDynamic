d246fc03dfe019169086ace8375838b7c20a0af0
CrioUser <crio-user@criodo.com>
2024-12-05 22:50:20 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”

--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -30,56 +30,21 @@ async function fetchCities() {
   // return newData;
 }
 
-let rowId = document.getElementById("data");
-console.log(rowId);
+// let rowId = document.getElementById("data");
+// console.log(rowId);
 //Implementation of DOM manipulation to add cities
-// function addCityToDOM(id, city, description, image) {
-//   let createCol = document.createElement("div");
-//   console.log(createCol);
-//   rowId.append(createCol);
-//   createCol.classList.add("col");
-//   createCol.style.height = "18rem";
-
-//   let anchorTag = document.createElement("a");
-//   anchorTag.setAttribute("id", id);
-//   anchorTag.setAttribute("href", `pages/adventures/?city=${id}`);
-//   createCol.append(anchorTag);
-
-//   let img = document.createElement("img");
-//   img.setAttribute("id", id);
-//   img.src = image;
-//   img.style.width = "15rem";
-//   img.style.height = "20rem";
-//   img.classList.add("rounded");
-//   anchorTag.append(img);
-
-//   let nestedCol = document.createElement("div");
-//   nestedCol.classList.add("placeInfo");
-//   nestedCol.innerHTML = "<h5></h5><p></p>";
-//   createCol.append(nestedCol);
-//   // Select the newly created elements directly
-//   let h5 = nestedCol.querySelector("h5");
-//   let p = nestedCol.querySelector("p");
-
-//   // Update their content
-//   h5.innerText = city;
-//   p.innerText = description;
-//   // <div class="d-flex flex-column justify-content-center align-items-center tile-text text-center">
-//   //           <h5 class="card-title text-white">Bengaluru</h5>
-//   //           <p class="card-text text-white">100+ PLACES</p>
-//   //         </div>
 function addCityToDOM(id, city, description, image) {
   let rowId = document.getElementById("data");
 
-  // Check if rowId exists
   if (!rowId) {
     console.error("Element with id 'data' not found.");
     return;
   }
-
   let createCol = document.createElement("div");
+  console.log(createCol);
   rowId.append(createCol);
   createCol.classList.add("col");
+  createCol.style.height = "18rem";
 
   let anchorTag = document.createElement("a");
   anchorTag.setAttribute("id", id);
@@ -87,7 +52,10 @@ function addCityToDOM(id, city, description, image) {
   createCol.append(anchorTag);
 
   let img = document.createElement("img");
+  img.setAttribute("id", id);
   img.src = image;
+  img.style.width = "15rem";
+  img.style.height = "20rem";
   img.classList.add("rounded");
   anchorTag.append(img);
 
@@ -95,15 +63,19 @@ function addCityToDOM(id, city, description, image) {
   nestedCol.classList.add("placeInfo");
   nestedCol.innerHTML = "<h5></h5><p></p>";
   createCol.append(nestedCol);
-
+  // Select the newly created elements directly
   let h5 = nestedCol.querySelector("h5");
   let p = nestedCol.querySelector("p");
+
+  // Update their content
   h5.innerText = city;
   p.innerText = description;
-}
-
+  // <div class="d-flex flex-column justify-content-center align-items-center tile-text text-center">
+  //           <h5 class="card-title text-white">Bengaluru</h5>
+  //           <p class="card-text text-white">100+ PLACES</p>
+  //         </div>
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
-
+}
 
 export { init, fetchCities, addCityToDOM };
